c1dd923619d86e17fafe8293ef59da2bfb25384d
Hardik Jatolia <98070776+hardikjatolia@users.noreply.github.com>
2023-07-13 22:06:13 +0530

    Update App.jsx
    updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/PageNotFound";
 function App() {
 const dispatch=useDispatch()
 const {url}=useSelector((state)=>state.home)
@@ -55,7 +54,6 @@ const {url}=useSelector((state)=>state.home)
         <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
     </Routes>
     <Footer />
 </BrowserRouter>
